d6732d85d43839f3f93d8cb9fa0d21ef0be9d027
vincent <fanyulu4w@gmail.com>
2019-02-20 21:28:09 +0800

    add zookeeper module

---
+++ zookeeper/src/main/java/org/vin/TestCase.java
@@ -0,0 +1,13 @@
+package org.vin;
+
+import org.junit.Test;
+
+public class TestCase {
+
+	@Test
+	public void run() {
+
+
+	}
+
+}
---
+++ zookeeper/src/main/java/org/vin/zookeeperTest.java
@@ -0,0 +1,40 @@
+package org.vin;
+
+import org.apache.zookeeper.*;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class zookeeperTest {
+	private static String connectString = "hadoop5:2181,hadoop6:2181,hadoop7:2181";
+	private static int sessionTimeout = 2000;
+	private ZooKeeper zkClient = null;
+
+
+	@Test
+	public void create() throws KeeperException, InterruptedException {
+		String msg = zkClient.create("/obstruct", "obfuscate".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+		System.err.println(msg);
+	}
+
+	@Before
+	public void init() throws Exception {
+
+		zkClient = new ZooKeeper(connectString, sessionTimeout, new Watcher() {
+
+			@Override
+			public void process(WatchedEvent event) {
+
+				// 收到事件通知后的回调函数（用户的业务逻辑）
+				System.out.println(event.getType() + "--" + event.getPath());
+
+				// 再次启动监听
+				try {
+					zkClient.getChildren("/", true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
